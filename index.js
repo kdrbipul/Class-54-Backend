fa8f459894912c96c6192a45f92e3238a1942504
kdrbipul <kdrbipul@gmail.com>
2023-10-25 00:48:32 +0600

    code kaj kore na

--- index.js
+++ index.js
@@ -1,6 +1,43 @@
 const express = require ("express");
+require('dotenv').config();
+const cors = require('cors');
 const app = express();
 const port = process.env.PORT || 5000;
+
+// midleware    
+
+app.use(cors());
+
+
+
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = `mongodb+srv://${process.env.DB_USSER}:${process.env.DB_PASS}@cluster0.4que1ds.mongodb.net/?retryWrites=true&w=majority`;
+console.log(uri);
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    const database = client.db("curdServer");
+    const userCollection = database.collection('user');
+    const user = {
+        userId : 1, userName: "abdul kader", age: 23
+    };
+    const result = await userCollection.insertOne(user);
+    console.log(result);
+  } finally {
+    
+  }
+}
+run().catch(console.dir);
+
 
 
 app.get("/", (req, res)=>{
